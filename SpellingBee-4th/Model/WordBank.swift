85d53bee9edd08998c2964aa5dbf0cef423eb7f2
Eric Hernandez <eands9@yahoo.com>
2019-01-14 11:19:38 -0600

    slavik5

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,18 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"bolshevik", sentence:""))
-        list.append(Word(word:"vampire", sentence:""))
-        list.append(Word(word:"sputnik", sentence:""))
-        list.append(Word(word:"knish", sentence:""))
-        list.append(Word(word:"cravat", sentence:""))
-        list.append(Word(word:"babushka", sentence:""))
-        list.append(Word(word:"soviet", sentence:""))
-        list.append(Word(word:"borzoi", sentence:""))
-        list.append(Word(word:"gopak", sentence:""))
-        list.append(Word(word:"cheka", sentence:""))
+        list.append(Word(word:"barabara", sentence:""))
+        list.append(Word(word:"aul", sentence:""))
+        list.append(Word(word:"balalaika", sentence:""))
+        list.append(Word(word:"kielbasa", sentence:""))
+        list.append(Word(word:"tchotchke", sentence:""))
+        list.append(Word(word:"barukhzy", sentence:""))
+        list.append(Word(word:"perestroika", sentence:""))
+        list.append(Word(word:"apparatchik", sentence:""))
+        list.append(Word(word:"commissar", sentence:""))
+        list.append(Word(word:"tokamak", sentence:""))
+        list.append(Word(word:"pogrom", sentence:""))
+        list.append(Word(word:"taiga", sentence:""))
+        list.append(Word(word:"beetewk", sentence:""))
     }
 }
